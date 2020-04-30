58b81f7db9f95c1314fc04df55a74e924c0e3e36
David Smolinski <dsmolinski123@gmail.com>
2020-04-30 16:01:19 -0400

    Updates

---
+++ precache-manifest.7ae88d92419aa906c3b93c870bf130c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6e50d3c88dc57ddc5ce9ba1d10fe6c",
+    "url": "/project_lister/index.html"
+  },
+  {
+    "revision": "7fe1108e3bd7d8918dcc",
+    "url": "/project_lister/static/css/main.b9ad8df0.chunk.css"
+  },
+  {
+    "revision": "75f3ff0376c27163261d",
+    "url": "/project_lister/static/js/2.c703a375.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project_lister/static/js/2.c703a375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe1108e3bd7d8918dcc",
+    "url": "/project_lister/static/js/main.db27c863.chunk.js"
+  },
+  {
+    "revision": "f64d029815ba74dc5436",
+    "url": "/project_lister/static/js/runtime-main.783a0bf7.js"
+  },
+  {
+    "revision": "7e6fea684cd4eddb070345b98733e164",
+    "url": "/project_lister/static/media/bee_bg.7e6fea68.jpg"
+  },
+  {
+    "revision": "c5ba7dfcc4627e343656355512a1b809",
+    "url": "/project_lister/static/media/head.c5ba7dfc.png"
+  },
+  {
+    "revision": "8260e3f99af22467943a04962ca314b4",
+    "url": "/project_lister/static/media/logo.8260e3f9.png"
+  }
+]);
